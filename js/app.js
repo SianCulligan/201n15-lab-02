5f6b7357238c442b7ae7df8a25df4eed5fd7296c
Sian Culligan <SianCulligan@gmail.com>
2019-08-13 21:00:55 -0700

    added question about my dogs name

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@
 var userName=prompt('What\'s your name?');
 alert('Hi ' + userName + '. Work experience is great and all, but I want you to get to know me.');
 
+var dogName = prompt('Wanna play a game? I initially tried to name my small dog \'Dogtor Who\', but got turned down. Instead I named him after a baby from Doctor Who in season 6 episode 12 - want to take a guess at his name?')
+dogName = dogName.toUpperCase();
+if (dogName === 'STORMAGEDDON'|| dogName === 'STORMAGEDDON DARK LORD OF ALL' ) {
+  alert('YES ' + userName + '. Clearly you\'ve learned that I\'m great at naming dogs. What\'s next?');
+} else {
+  alert('Nope - Stormageddon Dark Lork of All is his name. Clearly you\'ve learned that I\'m great at naming dogs. What\'s next?');
+}
+
 var zombieFlamingos = prompt('Take a stab at this - guess how many Zombie Flamingos I have in my front yard?');
 zombieFlamingos = zombieFlamingos.toLowerCase();
 if (zombieFlamingos === 2 || zombieFlamingos === 'two') {
